14bfa0004c9cfd52daecacaf4921a6119e005669
Loooooan <84080826+Loooooan@users.noreply.github.com>
2021-11-01 18:22:31 +0100

    Update Exponentiation rapide et naïve.py

--- DM1 Validir/Exponentiation rapide et naïve.py
+++ DM1 Validir/Exponentiation rapide et naïve.py
@@ -4,27 +4,29 @@ import matplotlib.pyplot as plt
 
 def puissanceN(q, n):
     tps_debut = time.time()
-    x=1
-    for k in range(1, n+1):
-        x=x*q
+    for i in range(10**5):
+        x=1
+        for k in range(1, n+1):
+            x=x*q
     tps_fin = time.time()
     return tps_fin - tps_debut
 
 def puissanceR(q, n):
     tps_debut = time.time()
-    r, x, e = 1, q, n
-    while e!=0:
-        if e%2==0:
-            x=x*x
-            e=e//2
-        else:
-            r=r*x
-            e=e-1
+    for i in range(10 ** 5):
+        r, x, e = 1, q, n
+        while e!=0:
+            if e%2==0:
+                x=x*x
+                e=e//2
+            else:
+                r=r*x
+                e=e-1
     tps_fin = time.time()
     return tps_fin - tps_debut
 
 q = 1.2345
-puissances = np.linspace(0, 5000000, 5)
+puissances = np.linspace(0, 500, 5)
 N = []
 R = []
 for elt in puissances:
@@ -33,5 +35,8 @@ for elt in puissances:
 
 plt.plot(puissances, N, label= "Naïf")
 plt.plot(puissances, R, label= "Rapide")
+plt.title("Temps de calcul des deux fonctions d'exponentiation (en secondes)")
+plt.xlabel("n")
+plt.ylabel("Temps de calcul (s)")
 plt.legend()
 plt.show()
